17dd043398af8829b8f38d6747870b7f2ef77f87
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-02 12:30:18 -0700

    Add state and onHover even

--- app.js
+++ app.js
@@ -16,11 +16,26 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      bold: false
+    };
+  }
+
+
+  onListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
   }
 
   render() {
+
+    var style = {
+      fontWeight: this.state.bold ? 'bold'  : 'normal'
+    }
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onHover={this.onListItemHover.bind(this)}>{this.props.item}</li>
     );
   };
 
