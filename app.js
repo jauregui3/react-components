0dc709fb1fa31c49d9107b73720a736564945e80
Juancarlos Jauregui <jauregui3@gmail.com>
2017-06-02 12:06:53 -0700

    Refactor Grocery List and create GroceryListItem

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// TODO
+var GroceryList = (prop) => (
+  <ul>
+  {
+    prop.groceryItems.map( (item) => (
+      <GroceryListItem item={item}/>
+    ))
+  }
+  </ul>
+);
+
+var GroceryListItem = (prop) => (
+  <li>{prop.item}</li>
+);
+
+// var Bread = () => (
+//   <li>Bread</li>
+// );
+
+// var Cheese = () => (
+//   <li>Cheese</li>
+// );
+
+ReactDOM.render(<GroceryList groceryItems={['Bread', 'Cheese']}/>, document.getElementById("app"));
